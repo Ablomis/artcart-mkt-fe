677044d15744c0400b4caa4785bb31ecd933b318
Anatolii <ablomis@gmail.com>
2022-04-17 22:22:13 -0700

    oops

--- functions/get-nfts/get-nfts.js
+++ functions/get-nfts/get-nfts.js
@@ -1,8 +1,6 @@
 const axios = require('axios').default;
 
 exports.handler = async (event, context, callback) => {
-  const data = JSON.parse(event.body)
-
   //getting auth token
   const responseToken = await axios.post("https://login.artcart.cloud/oauth/token", {
     grant_type: 'client_credentials',
