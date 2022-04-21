5180b67c66934acd49cd234deaa752945446ba37
Anatolii <ablomis@gmail.com>
2022-04-21 11:09:32 -0700

    corzz

--- functions/get-nfts/get-nfts.js
+++ functions/get-nfts/get-nfts.js
@@ -2,6 +2,17 @@ const axios = require('axios').default;
 
 exports.handler = async (event, context, callback) => {
   //getting auth token
+
+  callback(null, {
+    statusCode: 200,
+    body: responseNFT.data,
+    headers: {
+      "access-control-allow-origin": "*",
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Methods': '*',
+    },
+  });
+
   const responseToken = await axios.post("https://login.artcart.cloud/oauth/token", {
     grant_type: 'client_credentials',
     client_id: process.env.client_id,
@@ -14,8 +25,4 @@ exports.handler = async (event, context, callback) => {
   const responseNFT = await axios.get(`https://platform.artcart.cloud/api/transactional/nft`, 
   { headers: { "Authorization": `Bearer ${token}` } });
   console.log(responseNFT.data)
-  return { 
-    statusCode: 200,
-    body: JSON.stringify(responseNFT.data)
-  }
 };
