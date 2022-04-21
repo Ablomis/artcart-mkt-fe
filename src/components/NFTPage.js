e6342a4fda0748bcfeaa4b25fbcbe74fffd5b613
Anatolii <ablomis@gmail.com>
2022-04-21 09:50:08 -0700

    less div

--- src/components/NFTPage.js
+++ src/components/NFTPage.js
@@ -52,11 +52,11 @@ const NFTPage = () => {
     }
   
     return (
-      <div>
-        <Grid container spacing={2} item xs={4}>
-            {mapStructure(JSON.parse(JSON.stringify(nftData, null, 2)))}
-      </Grid>
-      </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2} item xs={4}>
+                {mapStructure(JSON.parse(JSON.stringify(nftData, null, 2)))}
+        </Grid>
+      </Box>
     );
   
   }
